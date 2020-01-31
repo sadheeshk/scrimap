b60b939aa922c40388d8cb82e2f80f0666c26fa1
sadheeshk <35288666+sadheeshk@users.noreply.github.com>
2020-01-31 19:59:58 +0530

    Update App.js

--- src/utube/App.js
+++ src/utube/App.js
@@ -28,3 +28,27 @@ render(){
 }
 
 }
+
+
+fetch('https://api.themoviedb.org/3/discover/movie?api_key=b2b4f13d3a253edb3015b11f882f0aa3&with_original_language=ta&language=ta&sort_by=release_date.asc&year='+this.props.year)
+   .then(res => res.json())
+   .then((data) => {
+     this.setState({ pagesCount: data['total_pages'] })
+     console.log(this.state.pagesCount)
+        console.log(this.state.pagesCount)
+
+
+  console.log("looping"+this.state.pagesCount);
+   for(var i=1;i<=this.state.pagesCount;i++){
+    //  console.log("looping"+i);
+     fetch('https://api.themoviedb.org/3/discover/movie?api_key=b2b4f13d3a253edb3015b11f882f0aa3&with_original_language=ta&language=ta&sort_by=release_date.asc&year='+this.props.year+'&page='+i)
+     .then(res => res.json())
+     .then((data) => {
+
+    // console.log(data['results'])
+
+       this.setState({ completeMovieList: this.state.completeMovieList.concat(data['results']) })
+
+     })
+   }   })
+    .catch(console.log)
